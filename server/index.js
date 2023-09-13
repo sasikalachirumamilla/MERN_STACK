4320cca60afa7c19feda69ce34dcb90301009383
Sasikala Chirumamilla <sasichirumamilla22@gmail.com>
2023-09-13 21:17:04 +0530

    Exploring

--- server/index.js
+++ server/index.js
@@ -7,7 +7,18 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import {fileURLToPath } from "url";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 dotenv.config();
@@ -26,10 +37,18 @@ const Storage = multer.diskStorage({
         cb(null,"public/assets");
     },
     filename:function(req,file,cb){
-    cb(null,file.originalname);
+    cb(null, file.originalname);
     }
 });
 const upload=multer({storage});
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 /*MONGOOSE SETUP*/
 const PORT=process.env.PORT || 6001;
 mongoose
